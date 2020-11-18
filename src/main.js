6f8ca35d896ace9e29c613409b5d910922c4946d
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-11-18 10:55:49 -0800

    Functioning Kanye Quote generator.
    Co-authored-by: Nick Hennessy <njhnny@gmail.com>
    Co-authored-by: Constantine Yakubovski <faustlarsen@gmail.com>

--- src/main.js
+++ src/main.js
@@ -4,5 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 $(document).ready(function() {
-
+  $('#insultButton').click(function() {
+    fetch('https://api.kanye.rest/')
+    .then(response => response.json())
+    .then(data => $('.showresponse').html(`<h3>This bite of Kanye logic: ${data.quote}. </h3>`));
+  
+  });
 });
